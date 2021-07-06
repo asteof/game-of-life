3d5f020badd0ca23b3d90e9748b47e4b28450c84
asteof <kirill.brezhnev02@gmail.com>
2021-07-06 23:34:00 +0300

    Added comments; Refactor: Global constants naming conventions; Optimization: changed innerHTML to textContent; Canvas continuous drawing optimization: cell changes states only if cursor has left cell and returned back;

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const epochLabel = document.getElementById('epoch');
 const gameOverLabel = document.getElementById('game-over');
 
+const CELL_AMOUNT = 10; //amount of cells in row and column
+//specifying different values for COLS and ROWS will lead to incorrect data display
+const COLS = CELL_AMOUNT;
+const ROWS = CELL_AMOUNT;
+
 //grid is a square
-const cellAmount = 10; //amount of cells in row and column
 let grid;
-//specifying different values will lead to incorrect data display
-const cols = cellAmount;
-const rows = cellAmount;
-let resolution;
+let resolution; //is used for resizing canvas and finding mouse position
 let epoch = 0;
 let intervalDelay = 600;
 let gameIsRunning = null; //variable used as interval for easy setting and clearing
 let isDrawing = false; //variable for continuous drawing, may be unused
+//determine previous position of mouse, used for optimization of continuous drawing
+let prevX, prevY;
 
+//creates two dimensional array
 const createArray = (cols, rows) => {
     let array = new Array(cols);
     for (let i = 0; i < array.length; i++) {
@@ -21,10 +25,11 @@ const createArray = (cols, rows) => {
     return array;
 }
 
+//creates 2D array containing initial values
 const createGrid = () => {
-    grid = createArray(cols, rows);
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
+    grid = createArray(COLS, ROWS);
+    for (let i = 0; i < COLS; i++) {
+        for (let j = 0; j < ROWS; j++) {
             grid[i][j] = 0; //fills arrays with 0
         }
     }
@@ -36,6 +41,7 @@ const createGrid = () => {
     console.table(grid);
 }
 
+//redraws canvas on window resizing
 const resizeCanvas = () => {
     const container = document.getElementById('container');
 
@@ -53,29 +59,32 @@ const resizeCanvas = () => {
     drawGrid(grid, ctx);
 }
 
+//used to reset game
 const clearGrid = () => {
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < COLS; i++) {
+        for (let j = 0; j < ROWS; j++) {
             grid[i][j] = 0; //fills arrays with 0
         }
     }
     // console.table(grid)
 }
 
+//fills grid with random values
 const randomizeGrid = () => {
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < COLS; i++) {
+        for (let j = 0; j < ROWS; j++) {
             grid[i][j] = Math.floor(Math.random() > 0.69 ? 1 : 0);
         }
     }
     console.table(grid);
 }
 
+//draws grid on canvas
 const drawGrid = (grid, ctx) => {
-    const cellSize = resolution / cellAmount;
+    const cellSize = resolution / CELL_AMOUNT;
 
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < COLS; i++) {
+        for (let j = 0; j < ROWS; j++) {
             let color;
             let strokeColor;
             if (grid[i][j] === 1) {
@@ -88,17 +97,21 @@ const drawGrid = (grid, ctx) => {
             ctx.fillStyle = color;
             ctx.strokeStyle = strokeColor;
             ctx.lineWidth = 2;
+            // (j * cellSize) is the X position to draw a rectangle
+            // (i * cellSize) is the Y position to draw a rectangle
+            // j comes first, because j stands for ROWS
             ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
             ctx.strokeRect(j * cellSize, i * cellSize, cellSize, cellSize);
         }
     }
 }
 
+//main logic for game, calculates next states for all grid
 const calculateNextGrid = (grid) => {
-    let nextGrid = createArray(cols, rows);
+    let nextGrid = createArray(COLS, ROWS);
 
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < COLS; i++) {
+        for (let j = 0; j < ROWS; j++) {
             let state = grid[i][j];    //current cell state
             let neighbours = countNeighbours(grid, i, j); //8 neighbours of cell in current grid
 
@@ -115,12 +128,13 @@ const calculateNextGrid = (grid) => {
     return nextGrid;
 }
 
+//counts sum of neighbour cells to apply game rules to specific cell
 const countNeighbours = (array, x, y) => {
     let sum = 0;
     for (let i = -1; i < 2; i++) {
         for (let j = -1; j < 2; j++) {
-            let col = (x + i + cols) % cols;
-            let row = (y + j + rows) % rows;
+            let col = (x + i + COLS) % COLS;
+            let row = (y + j + ROWS) % ROWS;
             sum += array[col][row];
         }
     }
@@ -128,6 +142,7 @@ const countNeighbours = (array, x, y) => {
     return sum;
 }
 
+//is used for calculating new game state and redrawing canvas
 const epochChange = () => {
     drawGrid(grid, ctx);
     let nextGrid = calculateNextGrid(grid);
@@ -139,8 +154,7 @@ const epochChange = () => {
         stopGame();
         gameOverLabel.style.display = 'flex';
     }
-    epochLabel.innerHTML = epoch.toString();
-    // requestAnimationFrame(epochChange)
+    epochLabel.textContent = epoch.toString();
 }
 
 const initialize = () => {
@@ -149,28 +163,36 @@ const initialize = () => {
     resizeCanvas();
 }
 
+//pauses the game, also used in reset function
 const stopGame = () => {
     clearInterval(gameIsRunning);
     gameIsRunning = null;
 }
 
+//resets game stats and grid to 0
 const resetGame = () => {
     stopGame();
     clearGrid();
     drawGrid(grid, ctx);
     epoch = 0;
-    epochLabel.innerHTML = epoch.toString();
-    startBtn.innerHTML = 'Start';
+    epochLabel.textContent = epoch.toString();
+    startBtn.textContent = 'Start';
     gameOverLabel.style.display = 'none';
 }
 
+//finds position of mouse relatively to canvas and changes state of cell
+//can be used as a click event listener on canvas element
+// IS USED in mousedown event, draws single cell
 const canvasClickHandler = (e) => {
+    console.log(isDrawing)
     gameOverLabel.style.display = 'none';
     let rect = canvas.getBoundingClientRect();
-    let x = Math.floor((e.clientX - rect.left) * cellAmount / resolution);
-    let y = Math.floor((e.clientY - rect.top) * cellAmount / resolution);
 
-    console.log(rect, x, y)
+    //(e.clientX - rect.left) is position of click relative to canvas
+    let x = Math.floor((e.clientX - rect.left) * (CELL_AMOUNT / resolution));
+    let y = Math.floor((e.clientY - rect.top) * (CELL_AMOUNT / resolution));
+
+    console.log(x, y, resolution, rect)
     if (grid[y][x] === 1) {
         grid[y][x] = 0;
         console.log(`x: ${x} y: ${y}\ngrid[${x}][${y}]: ${grid[x][y]} -> 0`);
@@ -178,14 +200,19 @@ const canvasClickHandler = (e) => {
         grid[y][x] = 1;
         console.log(`x: ${x} y: ${y}\ngrid[${x}][${y}]: ${grid[x][y]} -> 1`);
     }
-
+    prevX = x;
+    prevY = y;
     drawGrid(grid, ctx);
 }
-/**
-const startDrawing = () => {
-        if (!isDrawing)
-            isDrawing = true;
+/**/
+
+//mousedown event, sets drawing variable to true to start drawing
+const startDrawing = (e) => {
+    canvasClickHandler(e)
+    if (!isDrawing)
+        isDrawing = true;
 }
+
 const finishDrawing = () => {
     if (isDrawing)
         isDrawing = false;
@@ -195,20 +222,22 @@ const canvasDrawHandler = (e) => {
     if (isDrawing) {
         gameOverLabel.style.display = 'none'
         let rect = canvas.getBoundingClientRect()
-        let x = Math.floor((e.clientX - rect.left) * cellAmount / resolution);
-        let y = Math.floor((e.clientY - rect.top) * cellAmount / resolution);
-        console.log(x, y)
-        // let prevX = x;
-        // let prevY = y;
-        // let prevPoint = grid[x][y];
-        // if (x !== prevX || y !== prevY) {
+        let x = Math.floor((e.clientX - rect.left) * CELL_AMOUNT / resolution);
+        let y = Math.floor((e.clientY - rect.top) * CELL_AMOUNT / resolution);
 
-        if (grid[y][x] === 1) {
-            grid[y][x] = 0;
-        } else if (grid[y][x] === 0) {
-            grid[y][x] = 1;
+        // let prevPoint = grid[x][y];
+
+        if (prevX !== x || prevY !== y) {
+
+            console.log(x, y)
+            if (grid[y][x] === 1) {
+                grid[y][x] = 0;
+            } else if (grid[y][x] === 0) {
+                grid[y][x] = 1;
+            }
         }
-        // }
+        prevX = x;
+        prevY = y;
         drawGrid(grid, ctx);
     }
 }
@@ -219,13 +248,13 @@ const startBtnHandler = () => {
     if (gameIsRunning === null) {
         gameIsRunning = setInterval(epochChange, intervalDelay);
     }
-    startBtn.innerHTML = 'Start';
+    startBtn.textContent = 'Start';
 }
 
 const stopBtnHandler = () => {
     if (gameIsRunning !== null) {
         stopGame();
-        startBtn.innerHTML = 'Resume';
+        startBtn.textContent = 'Resume';
     }
 }
 
@@ -251,8 +280,8 @@ window.addEventListener('resize', resizeCanvas);
 
 const canvas = document.getElementById('field');
 const ctx = canvas.getContext('2d');
-canvas.addEventListener('click', canvasClickHandler);
-/*
+// canvas.addEventListener('click', canvasClickHandler);
+/**/
 canvas.addEventListener('mousedown', startDrawing)
 canvas.addEventListener('mouseup', finishDrawing)
 window.addEventListener('mouseup', finishDrawing)
@@ -282,5 +311,5 @@ slider.addEventListener('input', () => {
         clearInterval(gameIsRunning);
         gameIsRunning = setInterval(epochChange, intervalDelay);
     }
-    speedometer.innerHTML = intervalDelay.toString() + 'ms';
+    speedometer.textContent = intervalDelay.toString() + 'ms';
 })
